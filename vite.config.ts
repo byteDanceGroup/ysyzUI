06429c15f202b8bddef0c11a406361d350f794ba
Qianvenh <3294416164@qq.com>
2023-01-20 21:18:05 +0800

    钱文浩提交按钮demo

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import Markdown from 'vite-plugin-md'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue({
     include:[/\.md$/,/\.vue$/]
-  }),Markdown()],
+  }),Markdown(),vueJsx()],
   build:{
     lib:{
       entry:path.resolve(__dirname, 'packages/index.ts'),
