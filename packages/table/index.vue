982b66979bfc626af832467d42c0d4db91a0ecce
Qianvenh <3294416164@qq.com>
2023-02-20 11:40:56 +0800

    Table组件添加了几个接口并完成其API文档

--- packages/table/index.vue
+++ packages/table/index.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script lang="ts" setup>
-import { defineAsyncComponent, computed } from 'vue';
+import { defineAsyncComponent, computed, provide } from 'vue';
 const ysyzThead = defineAsyncComponent(() => import('./thead/index.vue'));
 const ysyzTbody = defineAsyncComponent(() => import('./tbody/index.vue'));
 
@@ -17,17 +17,17 @@ enum Size {
 interface Table {
 	data: Record<string, string>[],
 	columns: { title: string, key: string, width?: number }[],
-	stripe?: boolean, // * 是否显示间隔斑马纹
+	stripe?: boolean,
 	border?: boolean,
-	'show-header'?: boolean, // * 是否显示表头
+	hideHeader?: boolean,
+	size?: keyof typeof Size,
+	noDataText?: string,
+	ableHover?: boolean,
+	highlightRow?: boolean,
 	width?: number | string, // * 表格宽度，单位 px
 	height?: number | string, // * 表格高度，单位 px，设置后，如果表格内容大于此值，会固定表头
-	'max-height'?: number | string, // * 表格最大高度，单位 px，设置后，如果表格内容大于此值，会固定表头
-	'disable-hover'?: boolean, // * (此处改为，开启悬停高亮) 禁用鼠标悬停时的高亮
-	'highlight-row'?: boolean, // * 是否支持高亮选中的行，即单选
-	'row-class-name'?: boolean, // * 行的 className 的回调方法，传入参数：row：当前行数据 index：当前行的索引
-	size?: keyof typeof Size,
-	'no-data-text'?: string, // * 数据为空时显示的提示内容
+	maxHeight?: number | string, // * 表格最大高度，单位 px，设置后，如果表格内容大于此值，会固定表头
+	rowClassName?: boolean, // * 行的 className 的回调方法，传入参数：row：当前行数据 index：当前行的索引
 }
 
 const props = withDefaults(defineProps<Table>(), {
@@ -35,21 +35,15 @@ const props = withDefaults(defineProps<Table>(), {
 	columns: () => [],
 	stripe: false,
 	border: false,
-	'show-header': true,
-	width: 'auto',
-	height: undefined,
-	'max-height': undefined,
-	'disable-hover': false,
-	'highlight-row': false,
-	'row-class-name': undefined,
+	hideHeader: false,
+	ableHover: false,
+	highlightRow: false,
 	size: 'default',
-	'no-data-text': '暂无数据',
+	noDataText: '暂无数据',
+	width: 'auto', // *
+	height: undefined, // *
+	maxHeight: undefined, // *
 });
-
-const emits = defineEmits([
-
-]);
-
 
 const tableClassObject = computed(() => {
 	if (props.size in Size === false)
@@ -61,6 +55,12 @@ const tableClassObject = computed(() => {
 		'ysyz-table-large': Size[props.size] === Size.large,
 	}
 });
+
+provide('hideHeader', props.hideHeader);
+provide('stripe', props.stripe);
+provide('noDataText', props.noDataText);
+provide('ableHover', props.ableHover);
+provide('highlightRow', props.highlightRow);
 </script>
 
 <style lang="scss">
